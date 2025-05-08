dc92d10554b13bb0acce69c90e9e94838d8fc530
Militaseeee <70491463+Militaseeee@users.noreply.github.com>
2025-05-08 08:41:52 -0500

    Update entrenamiento_mod1_sem3.py

--- Semana_3/entrenamiento_mod1_sem3.py
+++ Semana_3/entrenamiento_mod1_sem3.py
@@ -1,31 +1,53 @@
-def agregar_productos(nombre, precio, cantidad):
-    
-    inventario_productos = []
-    
-    if nombre == "":
-        print("\n__________ ¡¡¡ ADVERTENCIA !!! __________")
-        print(f"\nEl nombre del producto no debe quedar vacio {nombre}\n")
-        return
-    
-    if precio < 0:
-        print("\n__________ ¡¡¡ ADVERTENCIA !!! __________")
-        print(f"\nEl número de precio debe ser mayor que {precio}\n")
-        return # Detiene la función aquí
-    
-    if cantidad < 0:
-        print("\n__________ ¡¡¡ ADVERTENCIA !!! __________")
-        print(f"\nEl número de cantidad debe ser mayor que {cantidad}\n")
-        return
-    
+# Función para evaluar si el nompre del producto no esté vacío
+def evaluar_producto_vacio(valor_ingresado: str, tipo: type):
+
+    validar_producto = False 
+
+    while not validar_producto:
+
+        valor = tipo(input(valor_ingresado)) # Mostrar mensaje del userio que ingresó
+        valor = valor.strip().lower()
+        if valor == "":
+            print("\n__________ ¡¡¡ ADVERTENCIA !!! __________")
+            print(f"\nEl nombre del producto no debe quedar vacío {valor}\n")
+        else:
+            return valor # Retorna la función aquí si esta vacio
+
+
+# Función para evaluar el precio y la cantidad donde el usuario no puede ingresar una letra
+def evaluar_tipo_numerico(numero_ingresado: str, tipo: type):
+
+    validar_num = False 
+
+    while not validar_num:
+
+        try:
+            valor = tipo(input(numero_ingresado)) # Convertir la entrada al tipo que se necesite
+            
+            if valor < 0:
+                print("\n_____________ ¡¡¡ ADVERTENCIA !!! _____________")
+                print(f"\nEl valor debe ser mayor que cero y pusiste: {valor}\n")
+            else:
+                return valor  # Retorna el valor solo si es válido
+        except ValueError:
+            print("\n--------------------------------------------")
+            print("Por favor, ingresa un valor numérico válido")
+            print("--------------------------------------------\n")
+
+
+# Función para agregar los productos
+def agregar_productos(inventario_productos, nombre, precio, cantidad):
+
     productos = {
         "nombre": nombre,
         "precio": precio,
         "cantidad": cantidad
     }
+    inv = {"nombre":(precio, cantidad)}
     inventario_productos.append(productos)
+    print(f"\nEl producto '{nombre}' ha sido agregado con éxito.\n")
     
-    print(inventario_productos)
-    return inventario_productos
+
 
 def menu():
     
@@ -33,6 +55,7 @@ def menu():
     intentos = 3  # Cantidad de números máximos que permite hacer el programa
     i = 0         # Contador de intentos para finalizar el programa
     comprobar: bool = True
+    inventario_productos = [] # Los poductos que tendre en el inventario
         
     print("-----------------------------------------------------------------")
     print("- - - Actividad: Gestión de inventarios con funciones y colecciones - - -")
@@ -57,15 +80,12 @@ def menu():
             print("  Elegiste la opción 1: Añadir productos ")
             print("*****************************************\n")
             
-            
-            
-            nombre = str(input("Ingresa el nombre del producto: "))
-            precio = float(input("Ingresa el precio del producto: "))
-            cantidad = int(input("Ingresa cuantos productos quiere llevar:"))
-            agregar_productos(nombre, precio, cantidad)
+            nombre = evaluar_producto_vacio("Ingresa el nombre del producto: ", str)
+            precio = evaluar_tipo_numerico("Ingresa el precio del producto: ", float)
+            cantidad = evaluar_tipo_numerico("Ingresa cuantos productos quiere llevar: ", int)
+            agregar_productos(inventario_productos,nombre, precio, cantidad)
                 
-
-            # comprobar = False
+            print(inventario_productos)
             continue
                     
         elif opc == "2":
@@ -100,7 +120,7 @@ def menu():
                 print("********************************************** \n")
 
                 print("**************************************************")
-                print(f"\n    S A L I E N D O   D E L   P R O G R A M A  \n")
+                print("\n    S A L I E N D O   D E L   P R O G R A M A  \n")
                 print("**************************************************")
 
                 comprobar = False
