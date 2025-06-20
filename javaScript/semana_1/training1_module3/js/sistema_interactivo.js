2e9cd64de195be1e51b1e47c6076de9310bd7cd0
Militaseeee <70491463+Militaseeee@users.noreply.github.com>
2025-06-19 23:51:06 -0500

    Add files via upload

--- javaScript/semana_1/training1_module3/js/sistema_interactivo.js
+++ javaScript/semana_1/training1_module3/js/sistema_interactivo.js
@@ -20,13 +20,27 @@ if (isNaN(age)) {
 
 function verifyAge() {
     let nameUser = document.getElementById('nameUser').value;
-    let age = parseInt(document.getElementById('age').value);
+    let age = document.getElementById('age').value;
 
-    if (nameUser.trim() === "" || isNaN(age) || age.trim() === "") {
+    if (nameUser.trim() === "" || isNaN(age) || age.trim() === "" || age < 0) {
 
-        setTimeout(() => {
-            alert("Please enter both your name and a valid age");
-        }, 1000); 
+        showToast("Please enter both your name and a valid age  ");
+        // console.log("ERROR!!! Please, enter a valid age in number !!!")
+        return;
+    } 
+
+    let ageCheck = parseInt(age);
+
+    if (ageCheck >= 0 && ageCheck <= 5) {
+        showToast(`Hi ${nameUser}, you are a baby! 👶`, "#ffccbc");
+    } else if (ageCheck >= 6 && ageCheck <= 10) {
+        showToast(`Hi ${nameUser}, you are very young! Study a lot to be a great student. 📚`, "#ffd54f");
+    } else if (ageCheck >= 11 && ageCheck <= 15) {
+        showToast(`Hi ${nameUser}, if you want to be a programmer, you can watch videos to learn what it is. 💡`, "#4fc3f7");
+    } else if (ageCheck >= 16 && ageCheck < 18) {
+        showToast(`Hi ${nameUser}, you are close to 18. Keep learning and practicing. 💻`, "#81c784");
+    } else if (ageCheck >= 18) {
+        showToast(`Hi ${nameUser}, you are an adult. Programming can help you build a good future! 🚀`, "#b39ddb");
     }
 }
 
@@ -34,4 +48,21 @@ function verifyAge() {
 function deleteInfo() {
     document.getElementById('nameUser').value = "";
     document.getElementById('age').value = "";
+}
+
+
+function showToast(message, backgroundColor = "#ff5f6d") {
+    Toastify({
+        text: message,
+        duration: 4000,
+        gravity: "top", 
+        position: "center",
+        close: true,
+        style: {
+            background: backgroundColor,
+            color: "#000",
+            borderRadius: "8px",
+            padding: "10px 5px"
+        }
+    }).showToast();
 }
