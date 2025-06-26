fd1a2013aa8c4035ea17f4a97068310e222e15b5
Militaseeee <70491463+Militaseeee@users.noreply.github.com>
2025-06-26 12:32:13 -0500

    Update gestion_datos.js

--- javaScript/semana_2/training2_module3/js/gestion_datos.js
+++ javaScript/semana_2/training2_module3/js/gestion_datos.js
@@ -23,69 +23,55 @@ const mapProducts = new Map([
 let nextId = Math.max(...Object.keys(products).map(Number)) + 1;
 
 
-// let conditionRepite = true
-
-// while (conditionRepite) {
-    // Ask the user if they want to add a new category
-    let userChoice 
-
-    while (userChoice !== 1 && userChoice !== 2) {
-        userChoice = prompt("Do you want to add a new category? Enter 1 for YES or 2 for NO:");
-        if (userChoice === "1") {
-
-            let conditionRepite = true
-
-            while (conditionRepite) {
-
-                // Request new category
-                let newCategory = prompt("Enter the name of a new category:");
-                console.log("New category entered:", newCategory);
-                
-                // Validate if it already exists
-                if (mapProducts.has(newCategory)) {
-                    alert("That category already exists!");
-                    console.log("Category already exists in Map.");
-                } else {
-                    let enteredProducts = prompt("Enter products for that category, separated by commas (e.g.: Tablet, Monitor):");
-                    let productList = enteredProducts.split(",").map(p => p.trim());
-
-                    // Add to map
-                    mapProducts.set(newCategory, productList);
-                    console.log(`Category ${newCategory} added with products:, productList`);
-
-                    // Add each product to the 'products' object
-                    // Create a new product with a random price, add it to products and the Set, then increment the ID
-                    productList.forEach(productName => {
-                        products[nextId] = {
-                            id: nextId,
-                            nameProduct: productName,
-                            price: Math.floor(Math.random() * 100 + 80) // Fake random price, only I use for put a price
-                        };
-                        setProducts.add(productName); // Add to set
-                        nextId++;
-                    });
-                    break;
-                }
-                const askAgain = prompt("Do you want to add other category? Enter 1 for YES or 2 for NO:");
-
-                if (askAgain !== "1") {
-                    conditionRepite = false
-                    break;
-                }
-                
-            }
+// Ask the user if they want to add a new category
+let userChoice 
+while (userChoice !== 1 && userChoice !== 2) {
+    userChoice = prompt("Do you want to add a new category? Enter 1 for YES or 2 for NO:");
+    if (userChoice === "1") {
+        let conditionRepite = true
+        while (conditionRepite) {
+            // Request new category
+            let newCategory = prompt("Enter the name of a new category:");
+            console.log("New category entered:", newCategory);
             
-        } else if (userChoice === "2") {
-            console.log("No new category was added");
-            // break;
-            conditionRepite = false
-            break;
-
-        } else {
-            alert("Invalid option. Please enter 1 (YES) or 2 (NO). The program will now stop");
+            // Validate if it already exists
+            if (mapProducts.has(newCategory)) {
+                alert("That category already exists!");
+                console.log("Category already exists in Map.");
+            } else {
+                let enteredProducts = prompt("Enter products for that category, separated by commas (e.g.: Tablet, Monitor):");
+                let productList = enteredProducts.split(",").map(p => p.trim());
+                // Add to map
+                mapProducts.set(newCategory, productList);
+                console.log(`Category ${newCategory} added with products:, productList`);
+                // Add each product to the 'products' object
+                // Create a new product with a random price, add it to products and the Set, then increment the ID
+                productList.forEach(productName => {
+                    products[nextId] = {
+                        id: nextId,
+                        nameProduct: productName,
+                        price: Math.floor(Math.random() * 100 + 80) // Fake random price, only I use for put a price
+                    };
+                    setProducts.add(productName); // Add to set
+                    nextId++;
+                });
+                break;
+            }
+            const askAgain = prompt("Do you want to add other category? Enter 1 for YES or 2 for NO:");
+            if (askAgain !== "1") {
+                conditionRepite = false
+                break;
+            }
         }
+    } else if (userChoice === "2") {
+        console.log("No new category was added");
+        // break;
+        conditionRepite = false
+        break;
+    } else {
+        alert("Invalid option. Please enter 1 (YES) or 2 (NO). The program will now stop");
     }
-// }
+}
 
 // Traverse the products object
 console.log("\n--- Object: products ---");
