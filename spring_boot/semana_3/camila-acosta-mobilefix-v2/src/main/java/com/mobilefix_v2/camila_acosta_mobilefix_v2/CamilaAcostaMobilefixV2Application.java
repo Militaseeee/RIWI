70912cc8501983bbc3f65959938a676cdf43659b
Militaseeee <camilitaacosta2001@gmail.com>
2025-11-04 21:39:54 -0500

    MVC

---
+++ spring_boot/semana_3/camila-acosta-mobilefix-v2/src/main/java/com/mobilefix_v2/camila_acosta_mobilefix_v2/CamilaAcostaMobilefixV2Application.java
@@ -0,0 +1,13 @@
+package com.mobilefix_v2.camila_acosta_mobilefix_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamilaAcostaMobilefixV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamilaAcostaMobilefixV2Application.class, args);
+	}
+
+}
---
+++ spring_boot/semana_3/camila-acosta-mobilefix-v2/src/test/java/com/mobilefix_v2/camila_acosta_mobilefix_v2/CamilaAcostaMobilefixV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilefix_v2.camila_acosta_mobilefix_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilaAcostaMobilefixV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
